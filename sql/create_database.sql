ccb338ec3aac913722cd8e8de600ae6faf9eaa32
Mohammad Hossein Mojtahedi <m.mojtahedi@rahkar.co>
2020-08-10 21:17:10 +0430

    init

---
+++ sql/create_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database.
+
+Note: ONLY development database;
+*/
+
+CREATE USER jijital SUPERUSER;
+CREATE DATABASE jijital OWNER jijital ENCODING 'utf-8';
